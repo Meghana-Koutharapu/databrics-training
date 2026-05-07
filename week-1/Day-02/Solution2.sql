4589f71e75c45022f93ddd02a3cc3ffe12356855
Meghana-Koutharapu <koutharapumeghana@gmail.com>
2026-05-07 20:25:42 +0530

    String Matching queries

--- week-1/Day-02/Solution2.sql
+++ week-1/Day-02/Solution2.sql
@@ -1,1 +1,19 @@
+ Select employees whose names start with 'J'
+       -> SELECT * FROM Employee
+          WHERE name LIKE 'J%';
 
+Select employees whose names end with 'e'
+      -> SELECT * FROM Employee
+         WHERE name LIKE '%e';
+
+Select employees whose names contain 'a'
+      -> SELECT * FROM Employee
+         WHERE name LIKE '%a%';
+
+Select employees whose names are exactly 9 characters long.
+      -> SELECT * FROM Employee
+         WHERE name LIKE '_________';
+
+Select employees whose names have 'o' as the second character.
+      -> SELECT * FROM Employee
+        WHERE name LIKE '_o%';
