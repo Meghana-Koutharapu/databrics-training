29990d071d2e63ac83b26d442a471992572c4863
Meghana-Koutharapu <koutharapumeghana@gmail.com>
2026-05-08 19:52:37 +0530

    Fix formatting of SQL query comments

--- week-1/Day-02/Solution2.sql
+++ week-1/Day-02/Solution2.sql
@@ -14,7 +14,7 @@
       -> SELECT * FROM Employee
          WHERE name LIKE '_________';
 
-10.Select employees whose names have 'o' as the second character.
+10. Select employees whose names have 'o' as the second character.
       -> SELECT * FROM Employee
         WHERE name LIKE '_o%';
 
