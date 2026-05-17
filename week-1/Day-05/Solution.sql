d9cb7111b9d24acd0bc1e602b6719e177d2603f1
Meghana-Koutharapu <koutharapumeghana@gmail.com>
2026-05-17 17:56:45 +0530

    Add SQL queries for employee compensation analysis

--- week-1/Day-05/Solution.sql
+++ week-1/Day-05/Solution.sql
@@ -1,1 +1,48 @@
+QUESTION 1: Employee Compensation Classification
+  
+1. Convert emp_name to Upper Case
+SELECT UPPER(emp_name) AS upper_name
+FROM employee_payments;
 
+2. Convert emp_name to Lower Case
+SELECT LOWER(emp_name) AS lower_name
+FROM employee_payments;
+
+3. Convert emp_name to Proper Case (Initcap / CamelCase)
+SELECT INITCAP(emp_name) AS proper_case_name
+FROM employee_payments;
+
+4. Calculate Total Income (base_salary + bonus) with NULL Safe
+SELECT 
+    emp_name,
+    base_salary + COALESCE(bonus,0) AS total_income
+FROM employee_payments;
+
+5. Round Total Income to Nearest Integer
+SELECT 
+    emp_name,
+    ROUND(base_salary + COALESCE(bonus,0)) AS rounded_income
+FROM employee_payments;
+
+6. Extract Joining Year
+SELECT 
+    emp_name,
+    EXTRACT(YEAR FROM joining_date) AS joining_year
+FROM employee_payments;
+
+7. Classify Employees using CASE
+SELECT 
+    emp_name,
+
+    CASE
+        WHEN EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM joining_date) > 7
+            THEN 'Senior'
+
+        WHEN EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM joining_date) 
+             BETWEEN 4 AND 7
+            THEN 'Mid'
+
+        ELSE 'Junior'
+    END AS employee_level
+
+FROM employee_payments;
