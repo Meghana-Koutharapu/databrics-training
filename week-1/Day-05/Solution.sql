c87606d65eaf84e28e9f90f8417ae28e5ca795ab
Meghana-Koutharapu <koutharapumeghana@gmail.com>
2026-05-17 18:08:06 +0530

    Add queries for order delivery delay and customer spending

--- week-1/Day-05/Solution.sql
+++ week-1/Day-05/Solution.sql
@@ -34,7 +34,6 @@ FROM employee_payments;
 
 
 QUESTION 2: Order Delivery Delay Analysis
-
 SELECT 
     order_id,
     UPPER(customer_name) AS customer_name,
@@ -53,5 +52,28 @@ SELECT
     END AS delivery_status
 
 FROM orders_delivery;
+
+
+QUESTION 3: Customer Spending Pattern
+
+SELECT 
+    CONCAT(
+        UPPER(LEFT(cust_name,1)),
+        LOWER(SUBSTRING(cust_name,2))
+    ) AS customer_name,
+
+    MONTHNAME(purchase_date) AS purchase_month,
+
+    ROUND(purchase_amount) AS rounded_purchase_amount,
+
+    ABS(purchase_amount) AS absolute_purchase_amount,
+
+    CASE
+        WHEN purchase_amount > 15000 THEN 'High Spender'
+        WHEN purchase_amount BETWEEN 8000 AND 15000 THEN 'Medium Spender'
+        ELSE 'Low Spender'
+    END AS spending_category
+
+FROM customer_spending;
 
 
